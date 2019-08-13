d859952df77f95f9d3e8d7b49a11ad42f1029ce6
Fergun <adammikol@gmail.com>
2019-08-13 20:20:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
-echo 'Witam na pierwszym repozytorium które rozpocząłem. Będe tutaj zamieszczać moją przygodę z programowaniem.';
-echo '<a href='student.agh.edu.pl/~mrczumix'>WEJDŹ</a>';
+echo("Witam na pierwszym repozytorium które rozpocząłem. Będe tutaj zamieszczać moją przygodę z programowaniem.");
 ?>
